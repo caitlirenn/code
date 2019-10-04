c71d81e8ef59e1be75875a2f93c9c7f495aab521
Renner <Caitlin.Renner@barings.com>
2019-10-04 13:53:09 +0100

    some new files

---
+++ assets/form_verification.js
@@ -0,0 +1,14 @@
+function getVal() {
+    var d = new Date();
+    var ad= document.getElementById("userinput").value;
+    var com = document.getElementById("comment").value;
+    document.write(d);
+    document.write( ad+ " says:  ");
+    document.write(com);
+}
+
+/*function printComment(){
+ var com = document.getElementById("comment").value;
+ document.write(com);
+ }
+ */
